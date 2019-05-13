eefd616a4c4d6155753d17ffa81bded0440751c6
bishes <bishesu@gmail.com>
2019-05-13 14:08:50 +0545

    navigation drawer completed

--- app/src/main/java/com/example/androidassignment/MainActivity.java
+++ app/src/main/java/com/example/androidassignment/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.androidassignment;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 Toolbar toolbar;
+DrawerLayout drawerLayout;
+NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,9 +24,49 @@ Toolbar toolbar;
 
         toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        toolbar.setTitle("Photo Genie");
 
-        
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.setDrawerListener(toggle);
+
+toggle.syncState();
+   }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+        drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else {
+            super.onBackPressed();
+        }
+
+
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        int id = menuItem.getItemId();
+        switch (id){
+            case R.id.login_id:
+                Toast.makeText(this, " redirecting to login/register", Toast.LENGTH_SHORT).show();
+            break;
+            case R.id.about_id:
+                Toast.makeText(this, " redirecting to about", Toast.LENGTH_SHORT).show();
+
+                break;
+            case  R.id.home_id:
+                Toast.makeText(this, " redirecting to home", Toast.LENGTH_SHORT).show();
+
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
 }
